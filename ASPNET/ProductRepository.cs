030a5ba7795b529c70343643a98a7474a51ec980
Andrew McMahon <andrewmcm26@gmail.com>
2020-02-25 09:46:41 -0600

    Added UpdateProduct()

--- ASPNET/ProductRepository.cs
+++ ASPNET/ProductRepository.cs
@@ -25,5 +25,17 @@ namespace ASPNET
             return _conn.QuerySingle<Product>("SELECT * from Products " +
                 "WHERE ProductID = @id;", new {id});
         }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE Products SET Name=@name, Price=@price, StockLevel=@stockLevel WHERE ProductID=@productID",
+                new
+                {
+                    productID = product.ProductID,
+                    name = product.Name,
+                    price = product.Price,
+                    stockLevel = product.StockLevel,
+                }); 
+        }
     }
 }
